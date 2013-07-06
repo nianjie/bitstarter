1aa82e907a3241dbd03486def04ba5fe4447af02
nianjie <ubuntu@ip-172-31-15-144.us-west-2.compute.internal>
2013-07-06 13:48:17 +0000

    implemented reading file system

--- web.js
+++ web.js
@@ -2,8 +2,31 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var cached = {};
+
+// a function that reads file specified.
+var readFile = function(fileName, encoding) {
+    if (!cached[fileName]) {
+	var fs = require('fs');
+	var out = fs.readFileSync(fileName, encoding);
+	console.log("read file : " + fs.realpathSync(fileName));
+	// either a buffer or a string
+	Buffer.isBuffer(out) ? cached[fileName] = out.toString() : cached[fileName] = out;
+    }
+    
+    return cached[fileName];
+};
+
+
+// var myBuffer = new Buffer(256);
+var indexfile = "index.html";
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fileContent = readFile(indexFile);
+
+    response.send(fileContent);
+    
+    console.log("responsed request from : " + request);
 });
 
 var port = process.env.PORT || 5000;
