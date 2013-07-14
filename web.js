4e5e5eab92986336bdb9b55bae4c42c15e3e28fd
nj <ubuntu@ip-172-31-15-144.us-west-2.compute.internal>
2013-07-14 06:30:58 +0000

    modify listening port to 8080.

--- web.js
+++ web.js
@@ -29,7 +29,7 @@ app.get('/', function(request, response) {
     console.log("responsed request from : " + request);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
