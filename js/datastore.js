c4a4cfd2c2eb7f7c59dc6355b7bcebba6246f732
jo <jo@jo.com>
2013-08-09 10:18:25 +0900

    add skeltons of js modules.

---
+++ js/datastore.js
@@ -0,0 +1,68 @@
+
+
+
+var datastore= new function () {
+
+
+
+
+/**
+* @namespace Datastore
+*/
+var self= this;
+
+
+
+
+/**
+* @field
+* @property {object}
+*/
+var data;
+
+/**
+* @field
+* @property {object (function)}
+*/
+var eventHandlers;
+
+/**
+* @field
+* @property {Firebase}
+*/
+var root;
+
+
+
+
+/**
+* @function
+* @property {void} Initialises this namespace
+*/
+var init;
+
+/**
+* @function
+* @property {Firebase}
+* @param {string} id (Optional; current user by default)
+*/
+var user;
+
+
+
+
+self.eventHandlers= {};
+self.root= new Firebase('https://napsterfm.firebaseio.com/');
+
+
+
+self.init= function () {
+
+
+
+};
+
+
+
+
+};
